dcef10b605fe2a8e23469dd2b1cb2fb6fce86679
thormengkheang <thormengkheang@gmail.com>
2024-01-26 13:11:29 +0700

    add test

--- __tests__/app/index.test.tsx
+++ __tests__/app/index.test.tsx
@@ -1,20 +1,29 @@
-import { renderRouter, screen } from "expo-router/testing-library";
+import { fireEvent, renderRouter, screen } from "expo-router/testing-library";
 
 import DetailsScreen from "../../app/details";
 import HomeScreen from "../../app/index";
 
+const renderAllRoutes = () => {
+  renderRouter(
+    {
+      index: HomeScreen,
+      details: DetailsScreen,
+    },
+    {
+      initialUrl: "/",
+    }
+  );
+};
 describe("Navigation", () => {
+  renderAllRoutes();
   it("has / path", async () => {
-    renderRouter(
-      {
-        index: HomeScreen,
-        details: DetailsScreen,
-      },
-      {
-        initialUrl: "/",
-      },
-    );
+    expect(screen).toHavePathname("/");
+  });
 
-    expect(screen).toHavePathname("/home");
+  it("has /details path", async () => {
+    renderAllRoutes();
+    const detail = await screen.findByRole("link", { name: "Go to Details" });
+    fireEvent.press(detail);
+    expect(screen.queryByText("Update the title")).toBeOnTheScreen();
   });
 });
